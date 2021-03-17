0d8077ffc2394e9d52fbe490b9c8de9097dfff87
najaflou <41740786+najaflou@users.noreply.github.com>
2021-03-17 23:49:35 +0300

    Initial Commit

---
+++ assignment5.py
@@ -0,0 +1,21 @@
+#importing required libraries
+import pandas as pd
+import numpy as np
+
+#Reading CSV file into a dataframe called df2
+df2= pd.read_csv(r"C:\Users\najaf\Downloads\country_vaccination_stats.csv")
+
+#will get a list of name of the countries in this dataframe
+CountryList= df2.country.unique()
+
+#Creating an empty dataframe to append results.
+Finaldf = pd.DataFrame() 
+
+for cntry in CountryList:
+    CountryMinValue= (df2[df2.country ==cntry]).min()["daily_vaccinations"]
+    Tempdf= (df2[df2.country ==cntry]).fillna(CountryMinValue)
+    Finaldf=Finaldf.append(Tempdf)
+
+#will groupby countries and find their means, then will show top 3 with highest mean
+Finaldf.groupby("country").daily_vaccinations.mean().nlargest(3)
+
